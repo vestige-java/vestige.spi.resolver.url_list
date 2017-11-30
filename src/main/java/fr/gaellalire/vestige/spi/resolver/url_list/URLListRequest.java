0c2370c8e91187980918da926760ff8aea4cc500
Gaël Lalire <gael.lalire@laposte.net>
2017-11-30 19:43:15 +0100

    import

---
+++ src/main/java/fr/gaellalire/vestige/spi/resolver/url_list/URLListRequest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.gaellalire.vestige.spi.resolver.url_list;
+
+import java.net.URL;
+
+import fr.gaellalire.vestige.spi.job.JobHelper;
+import fr.gaellalire.vestige.spi.resolver.ResolvedClassLoaderConfiguration;
+import fr.gaellalire.vestige.spi.resolver.ResolverException;
+
+/**
+ * @author Gael Lalire
+ */
+public interface URLListRequest {
+
+    void addURL(URL url);
+
+    void addExports(String moduleName, String packageName);
+
+    void addOpens(String moduleName, String packageName);
+
+    ResolvedClassLoaderConfiguration execute(JobHelper jobHelper) throws ResolverException;
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module fr.gaellalire.vestige.spi.resolver.url_list {
+
+    requires transitive fr.gaellalire.vestige.spi.resolver;
+
+    requires transitive fr.gaellalire.vestige.spi.job;
+
+    exports fr.gaellalire.vestige.spi.resolver.url_list;
+
+}
